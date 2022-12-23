f5c98bf082d8483a5f4e6f9bd27967aec1ccd526
Kittiwin <kittiwinktb@gmail.com>
2022-12-23 16:00:40 +0700

    lab9_3 complete

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -13,6 +13,7 @@
 */
 #include <iostream>
 #include <string>
+#include <iomanip>
 using namespace std;
 
 
@@ -26,27 +27,29 @@ int main(){
         cin >> H;
         if(H < 100){
             C = "Chopper";
-        }else if(H <= 180){
+        }else if(H < 180){
             C = "Usopp";
         }else{
             cout << "Enter your bounty: ";
             cin >> B;
-        }
-        if(B >= 1100000000){
+        if(B > 1100000000){
                 C = "Zoro";
             }else{
                 C = "Sanji";
             }
+
+        }
+        
     }else if(A <= 60){
         cout << "Enter your bounty: ";
         cin >> B;
-        if(B >= 500000000){
+        if(B > 500000000){
             C = "Jinbe";
         }else{
             C = "Franky";
         }
     }else {
-        C = "Blook";
+        C = "Brook";
     }
     cout << "Your character = " << C;
     return 0;
